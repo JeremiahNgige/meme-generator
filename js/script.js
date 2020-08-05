03ec0551a27571ad7553e093f561d6fe80524096
Jeremiah Brian <brianjeremy123789@gmail.com>
2020-08-05 22:58:22 +0300

    text display canvas

--- js/script.js
+++ js/script.js
@@ -1,28 +1,52 @@
-const inpFile = document.getElementById("inpFile");
+const inpFile      = document.getElementById("inpFile");
 var previewContainer = document.getElementById("imgPrev");
-var img1 = new Image();
-const previewText  = previewContainer.querySelector(".imgprevTxt");
-
+var img1           = new Image();
+let topButton      = document.getElementById("btnTop");
+let bottomButton   = document.getElementById("btnBottom");
+var ctx = previewContainer.getContext('2d');
 
 inpFile.addEventListener('change', function(){
      const file = this.files[0];
      console.log(file)
-     if(file && previewContainer.getContext){
+     if(file){
          const reader =new FileReader();
-         var ctx = previewContainer.getContext('2d');
 
          reader.addEventListener('load', function(){
              console.log(this);
              
              img1.src= this.result;
-             
-              
-             img1.onload = function () {
-                //draw background image
-                ctx.drawImage(img1, 0, 0, img1.width, img1.height,0,0,previewContainer.width, previewContainer.height);
-            };
+             imageDraw();
          });
          reader.readAsDataURL(file);
      }
-
 });
+function generateMeme(img, topText, bottomText){
+    this.img =img1;
+    if (previewContainer.getContext){
+        img1.onload = function () {
+               
+            ctx.clearRect(0,0 ,previewContainer.width, previewContainer.height);  
+            //draw background image
+            ctx.drawImage(img1, 0, 0, img.width, img.height,
+                                0, 0, previewContainer.width, previewContainer.height);
+            
+        }
+    }
+}
+let generate = document.getElementById('generate');
+generate.addEventListener('click', showTxt );
+ function showTxt(){
+    imageDraw();
+    var topTxt = document.getElementById('topTxt').value;
+    alert(topTxt);
+   
+    //let text = topTxt.toUpperCase();
+    ctx.font = "30px Arial";
+    ctx.textAlign= "center";
+    ctx.fillStyle = "white";
+    ctx.strokeStyle= "black ";
+    ctx.fillText(topTxt,previewContainer.width*0.2,previewContainer.height*0.15);
+}
+let generateMeme = function (){
+    
+}

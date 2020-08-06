12569874df10c62204597b659be699a49bf68fff
[Allantuikong] <allantuix@gmail.com>
2020-08-06 11:50:56 +0300

    Add functionality to write text

--- js/script.js
+++ js/script.js
@@ -1,50 +1,72 @@
-const inpFile      = document.getElementById("inpFile");
-var previewContainer = document.getElementById("imgPrev");
-var img1           = new Image();
-let topButton      = document.getElementById("btnTop");
-let bottomButton   = document.getElementById("btnBottom");
-var ctx = previewContainer.getContext('2d');
+function fileSelect(event) {
+    let file = event.target.files[0];
 
-inpFile.addEventListener('change', function(){
-     const file = this.files[0];
-     console.log(file)
-     if(file){
-         const reader =new FileReader();
+    let reader = new FileReader();
+    reader.onload = function(fileObject) {
+        let data = fileObject.target.result;
 
-         reader.addEventListener('load', function(){
-             console.log(this);
-             
-             img1.src= this.result;
-             imageDraw();
-         });
-         reader.readAsDataURL(file);
-     }
-});
-function imageDraw(){
-    
-    if (previewContainer.getContext){
-        img1.onload = function () {
-               
-            ctx.clearRect(0,0 ,previewContainer.width, previewContainer.height);  
-            //draw background image
-            ctx.drawImage(img1, 0, 0, img1.width, img1.height,
-                                0, 0, previewContainer.width, previewContainer.height);
-            
+        // Create an image object
+        let image = new Image();
+        image.onload = function() {
+
+            window.imageSrc = this;
+            redrawMeme(window.imageSrc, null, null);
         }
-    }
+
+        // Set image data to background image.
+        image.src = data;
+        console.log(fileObject.target.result);
+    };
+    reader.readAsDataURL(file)
 }
-let generate = document.getElementById('generate');
-generate.addEventListener('click', showTxt );
- function showTxt(){
-    imageDraw();
-    var topTxt = document.getElementById('topTxt').value;
-    alert(topTxt);
-   
-    //let text = topTxt.toUpperCase();
-    ctx.font = "20px Roboto";
-    ctx.textAlign= "center";
-    ctx.fillStyle = "white";
-    ctx.strokeStyle= "black ";
-    ctx.fillText(topTxt,previewContainer.width*0.2,previewContainer.height*0.15);
+
+window.topLineText = "";
+window.bottomLineText ="";
+let input1 = document.getElementById('topLineText');
+let input2 = document .getElementById('bottomLineText');
+input1.oninput = textChangeListener;
+input2.oninput = textChangeListener;
+document.getElementById('file').addEventListener('change',fileSelect, false);
+
+function textChangeListener (event){
+    let id = event.target.id;
+    let text = event.target.value;
+
+    if (id == "topLineText"){
+        window.topLineText = text;
+    }
+    else {
+        window.bottomLineText = text;
+    }
+    redrawMeme(window.imageSrc, window.topLineText, window.bottomLineText);
+
+}
+
+function redrawMeme(image,topLine,bottomLine) {
+    let canvas = document.querySelector('canvas');
+    let context = canvas.getContext("2d");
+    if (image!=null){
+        context.clearRect(0,0,canvas.width,canvas.height)
+        context.drawImage(image,0,0,canvas.width,canvas.height);
+    }
+
+    context.font = "36pt Sans-serif";
+    context.textAlign = "center";
+    context.fillStyle = "white";
+    context.strokeStyle = "black";
+    context.lineWidth = "3";
+
+
+    if (topLine!=null){
+        context.fillText(topLine,canvas.width/2,50);
+        context.strokeText(topLine,canvas.width/2,50);
+    }
+
+    if (bottomLine!=null){
+        context.fillText(bottomLine,canvas.width/2,canvas.height-20);
+        context.strokeText(bottomLine,canvas.width/2,canvas.height-20);
+    }
+
+
 }
 

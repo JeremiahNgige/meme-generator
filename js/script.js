0215d85dfb22abed08a6995673897836d7a94b9a
Jeremiah Brian <brianjeremy123789@gmail.com>
2020-08-07 04:51:52 +0300

    final project design

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//read the inputted image and declare it as the result in the image value in the redrawMeme fn
+
 function fileSelect(event) {
     let file = event.target.files[0];
 
@@ -15,10 +17,10 @@ function fileSelect(event) {
 
         // Set image data to background image.
         image.src = data;
-        console.log(data);
     };
     reader.readAsDataURL(file)
 }
+//window to represent the canvas window which is being targetted
 window.fontColor= "";
 window.topLineText = "";
 window.bottomLineText ="";
@@ -40,7 +42,7 @@ document.getElementById('file').addEventListener('change',fileSelect, false);
 function textChangeListener (event){
     let id = event.target.id;
     let text = event.target.value;
-    let indexnu = event.target.selectedIndex;
+    let indexNo = event.target.selectedIndex;
 
     if (id == "topLineText"){
         window.topLineText = text;   
@@ -52,7 +54,7 @@ function textChangeListener (event){
         window.fontSize = text;
     } 
     else {
-        switch(indexnu){
+        switch(indexNo){
             case 0:
                 window.fontColor = text;
             break;
@@ -84,28 +86,29 @@ function textChangeListener (event){
     redrawMeme(window.imageSrc, window.topLineText, window.bottomLineText,window.fontColor,window.fontSize);
 
 }
+//declaration ofa function that collects the image input,input top text,bottom text, font size and font color
+//and then draws the them all in a heirachy.
 
 function redrawMeme(image,topLine,bottomLine,fontColor,fontSize) {
     let canvas = document.querySelector('canvas');
     let ctx = canvas.getContext("2d");
+    
     if (image!=null){
         ctx.clearRect(0,0,canvas.width,canvas.height)
         ctx.drawImage(image,0,0,canvas.width,canvas.height);
     }
-
-
-
+//draw the top text with a stroke 
     if (topLine!=null){
         ctx.fillText(topLine,canvas.width/2,50);
         ctx.strokeText(topLine,canvas.width/2,50);
     }
-
+//draw the bottom text with a stroke
     if (bottomLine!=null){
         ctx.fillText(bottomLine,canvas.width/2,canvas.height-20);
         ctx.strokeText(bottomLine,canvas.width/2,canvas.height-20);
     }
 
-    
+//text style for both bottom and top text
     ctx.font =  fontSize +"pt Sans-serif";
     ctx.textAlign = "center";
     ctx.fillStyle = fontColor;
@@ -113,10 +116,8 @@ function redrawMeme(image,topLine,bottomLine,fontColor,fontSize) {
     ctx.lineWidth = "2";
 
 }
-
+//onclicking download meme replace the png version of the meme to octate-stream after a new window opens 
 buttonSave.onclick = function saveFile() {
     window.open(document.querySelector('canvas').toDataURL().replace("image/png", "image/octet-stream"));
     
  }
- //var image = document.querySelector('canvas').toDataURL("image/png").replace("image/png", "image/octet-stream");  
-     //window.location.href=image;
